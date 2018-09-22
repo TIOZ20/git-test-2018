2fea303c347f9586d0b8fd18ba46d63b2b9615d4
Jenya <jenyanejvert2018@gmail.com>
2018-09-22 13:17:39 +0300

    add new event js file

--- js.js
+++ js.js
@@ -2,4 +2,13 @@ $(function (){
     $("a").click(function(){
         alert("hello man i am jQuery method click");
     });
+
+    $("a").focus(function(){
+        alert("hello i am a $ method focus");
+    });
+
+    $("a").blur(function(){
+        alert("hello i am a $ method blur");
+    });
+
 });
